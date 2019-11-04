83125d24006bd48e66affccd11ec122e244e192e
jelm <jelm@st-andrews.ac.uk>
2019-11-04 16:10:11 +0000

    Changed to JSON

--- cs4099/website/main/script.js
+++ cs4099/website/main/script.js
@@ -1,17 +1,29 @@
 function draw() {
     // read through db of names
     console.log("hello");
-    var rows = $.csv.toObjects("../../database/words.csv");
+    // var rows = csv.toObjects("../../database/words.csv");
+    let file = new File([""], "/cs/home/jelm/Documents/CS4099-master/cs4099/database/words.json");
+
+    let reader = new FileReader(file);
+    reader.readAsArrayBuffer();
+    // reader.onload = readSuccess;
+    
+    console.log(file);
+    var array = JSON.parse(file);
 
     // generate div for each name
-    for (var i = 0; i <  rows.length; i++) {
+    console.log("before for");
+    for (var i = 0; i < array.medicines.length; i++) {
         // read db and add to rows arr
+        console.log("in for");
+        var medicine = array.medicines[i];
 
         // write divs to page
         var div = document.createElement("div");
         div.setAttribute("id", "element" + i);
 
-        document.getElementById('element' + i).innerHTML += rows[i];
+        document.getElementById('element' + i).innerHTML += medicine.name;
+        console.log(medicine.name);
         document.getElementById("list").appendChild(div);
     }
 
