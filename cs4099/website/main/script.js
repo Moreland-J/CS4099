becd02066dc131ca634100eb4f67ee16062436be
jelm <jelm@st-andrews.ac.uk>
2019-11-26 11:00:33 +0000

    Read in CSV

--- cs4099/website/main/script.js
+++ cs4099/website/main/script.js
@@ -1,16 +1,18 @@
-var csv = "../../database/db.csv";
+// var file = '../../database/db.csv';
+var file = 'cs/home/jelm/Documents/CS4099-master/cs4099/database/db.csv';
+var csv;
 // must define this as a file
 var json;
 let divs = [];
 
 draw = function() {
     // read through db of names
-    console.log("hello");
 
-    if(window.FileReader) {
+    if (window.File && window.FileReader && window.FileList && window.Blob) {
         console.log(true);
     }
 
+    csv = readFile();
     json = csvToJson();
     console.log("json made");
     
@@ -19,6 +21,50 @@ draw = function() {
     // go through 
 }
 
+function readFile() {
+    // XMLHttpRequest has ben deprecated
+    var f = file;
+    
+    console.log("read");
+    if (f) {
+        console.log("2");
+        var reader = new FileReader();
+        reader.onload = function(e){
+            console.log("3");
+            var contents = e.target.result;
+            console.log("Got the file.\n" +
+                        "Name: " + f.name +
+                        "\nSize: " + f.size + " bytes" +
+                        "\nstarts with: " + contents.substr(0, contents.indexOf("\n")));
+        }
+        reader.readAsText(f);
+        csv = reader.result;
+        csvToJson();
+    }
+}
+
+function read(evt) {
+    var f = evt.target.files[0];
+    
+    console.log("read");
+    if (f) {
+        console.log("2");
+        var reader = new FileReader();
+        reader.onload = function(e){
+            console.log("3");
+            var contents = e.target.result;
+            console.log("Got the file.\n" +
+                        "Name: " + f.name +
+                        "\nSize: " + f.size + " bytes" +
+                        "\nstarts with: " + contents.substr(0, contents.indexOf("\n")));
+        }
+        reader.readAsText(f);
+        csv = reader.result;
+        csvToJson();
+    }
+}
+
+document.getElementById('fileinput').addEventListener('change', read, false);
 
 function csvToJson() {
     var lines = csv.split("\n");
@@ -40,3 +86,6 @@ function csvToJson() {
 
     return JSON.stringify(result);
 }
+
+// works reading in file when choose file input is selected
+// doesn't work when using readFile from onload
