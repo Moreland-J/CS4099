1cacc0ebacdf430cd7489d3049f9ea1491c60449
Zyxwee <jelm@st-andrews.ac.uk>
2020-01-29 15:14:58 +0000

    More recordings, repeat playback option

--- cs4099/medPro1.0.py
+++ cs4099/medPro1.0.py
@@ -11,12 +11,50 @@ from pydub.playback import play
 
 running = True
 selected = 1
-word = "null"
 db = []
 # selected acts as db index
 
+# CONSTANT RUNNING INTERACTION
+def run():
+    readDB()
+
+    while (running):
+        userIn = input("Type the word you'd like to hear or 'exit' to leave the program.\n")
+        if (userIn == "exit"):
+            sys.exit()
+        elif (db.__contains__(userIn)):
+            word = userIn
+
+            # break into syllables
+            slicer = Slice(word, 3)
+            slicer.slice()
+            count = 0
+            for morphs in slicer.morphemes:
+                if count < len(slicer.morphemes) / 2 and count != len(slicer.morphemes) / 2 - 1:
+                    print(morphs + " - ", end = "")
+                elif count < len(slicer.morphemes) / 2:
+                    print(morphs)
+                count += 1
+
+            # https://pythonbasics.org/python-play-sound/
+            try:
+                playback(word)
+                userIn = input("Would you like to attempt the word? Y/N or would you like a repeat? R")
+                while (userIn == 'r'):
+                    playback(word)
+                    userIn = input("Would you like to attempt the word? Y/N or would you like a repeat? R ")
+                if (userIn == 'y'):
+                    listen(word)
+            except:
+                print("Audio recording does not exist for this word.")
+        else:
+            continue
+    return
+
+# READ DATABASE CSV FILE AND PRINT TO INTERFACE
 def readDB():
     # https://realpython.com/python-csv/
+    print()
     with open('database/db.csv') as file:
         reader = csv.reader(file, delimiter = ',')
         count = 0
@@ -30,35 +68,14 @@ def readDB():
     print()
     return
 
-# CONSTANT RUNNING INTERACTION
-def run():
-    global word
-    while (running):
-        userIn = input("Type the word you'd like to hear or 'exit' to leave the program.\n")
-        if (userIn == "exit"):
-            sys.exit()
-        elif (db.__contains__(userIn)):
-            word = userIn
-
-            # break into syllables
-            slicer = Slice(word, 8)
-            slicer.slice()
-            for morphs in slicer.morphemes:
-                print(morphs)
-
-            # https://pythonbasics.org/python-play-sound/
-            song = AudioSegment.from_wav("database/" + userIn + ".wav")
-            play(song)
-            userIn = input("Would you like to attempt the word? Y/N ")
-            if (userIn == 'y'):
-                listen()
-        else:
-            continue
-    return
+# PLAY RECORDING FOR USER
+def playback(word):
+    song = AudioSegment.from_wav("database/" + word + ".wav")
+    play(song)
 
 # USE API TO LISTEN TO USER VOICE
 # https://pythonprogramminglanguage.com/speech-recognition/
-def listen():
+def listen(word):
     rec = sr.Recognizer()
     with sr.Microphone() as source:
         print("Speak")
@@ -66,7 +83,7 @@ def listen():
     try:
         print("Comparing")
         text = rec.recognize_google(audio)
-        compare(text)
+        compare(word, text)
         return
     except sr.UnknownValueError:
         print("Could not understand audio")
@@ -74,12 +91,11 @@ def listen():
         print("API error; ".format(e))            
 
 # COMPARE DB WORD TO USER ATTEMPT
-def compare(userWord):
+def compare(word, userWord):
     if (word == userWord):
         print("Correct!")
     else:
         print("Not quite.")
     return
 
-readDB()
 run()
