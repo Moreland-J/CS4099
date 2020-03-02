ef13c38e7d64e772c184a1fcded0eb0978f86c52
Zyxwee <jelm@st-andrews.ac.uk>
2020-03-02 10:38:01 +0000

    Wiki Scraping and Syllable CSV

--- cs4099/wikiScraping.py
+++ cs4099/wikiScraping.py
@@ -1,34 +1,109 @@
 import requests
 import pandas as pd
+import re
 import csv
 
 from bs4 import BeautifulSoup
 from IPython.display import display_html
 from lxml import etree
 
-url = requests.get('https://en.wikipedia.org/wiki/List_of_medical_roots,_suffixes_and_prefixes').text
-soup = BeautifulSoup(url, 'lxml')
-# print(soup.prettify())
+fixes = []
 
-print("---------------------------------------------------------------------------------------------------------------------------------------------------------------------------")
+def scrape():
+    url = requests.get('https://en.wikipedia.org/wiki/List_of_medical_roots,_suffixes_and_prefixes').text
+    soup = BeautifulSoup(url, 'lxml')
+    tables = soup.findAll('table')
 
-tables = soup.findAll('table')
-# print(tables)
+    print("---------------------------------------------------------------------------------------------------------------------------------------------------------------------------\n")
 
-print("---------------------------------------------------------------------------------------------------------------------------------------------------------------------------")
+    string = str(soup)
+    for line in string.splitlines():
+        if bool(re.search('row', line)):
+            start = line.find('<b>')
+            end = line.find('</b>')
+            print(line[start:end][3:])
+            fixes.append(line[start:end][3:])
 
-# parser = etree.HTMLParser()
-# tree = etree.fromstring(tables, parser)
-# results = tree.xpath('//tr/td [position() = 1]')
+def clean():
+    clean = False
+    count = 0
+    print("cleaning")
+    while not clean:
+        # NEW SECTION FOR GOING THROUGH EACH OF THE FIXES
+        for num, fix in enumerate(fixes, start = 0):
+            # print("HERE")
+            # print(str(count) + " " + str(len(fixes)))
+            # REMOVES NON-FIXES
+            if '-' not in fix:
+                fixes.remove(fix)
+                count = 0
 
-# for r in results:
-#     print(r.text)
+            # TACKLE LISTS FIRST
+            # ADD EXTRAS TO END OF FIXES (or insert)
+            elif "," in fix:
+                # print("commas")
+                l = re.compile(",").split(fix)
+                fixes.remove(fix)
+                for i in range(0, len(l)):
+                    fixes.append(l[i])
+                count = 0
+            else:
+                count += 1
 
-display_html(tables, raw=True)
+            # SECONDLY, HYPERLINKS
+            # DETECT href AND DETECT ">[fix]</a>" REPEAT <b> PRACTISE
+            if "href" in fix:
+                # print("href")
+                start = fix.find('>')
+                end = fix.find('</a>')
+                fixes[num] = fix[start:end][1:0]
+                count = 0
+                # SPECIAL CASE WHERE BRACKETS AFTER HYPERLINK </a>
+            else:
+                count += 1
 
-print("\nrows\n")
+            # THEN HANDLE BRACKETS WITH ONLY ONE LETTER INSIDE
+            # REMOVE BRACKETS AND ADD BOTH WITH AND WITHOUT ONE LETTER TO LIST
+            if "(" in fix:
+                # print("single")
+                start = fix.find('(')
+                end = fix.find(')')
+                fixes.append(fix + fix[start:end][1:0])
+                if fix.startswith('-'):
+                    fixes[num] = fix[0:start]
+                elif fix.endswith('-'):
+                    fixes[num] = fix[0:start] + '-'
+                count = 0
+            else:
+                count += 1
 
-for row in tables:
-    print(row[0])
+        # MARK TO EXIT WHILE LOOP IF ALL ELEMENTS OF LIST ARE CLEAR OF MISTAKES
+        if count == len(fixes):
+            clean = True
 
+        for fix in fixes:
+            print(fix)
+
+def writeCSV():
+    legit = []
+    for fix in fixes:
+        if "," in fix:
+            pass
+        elif "<" in fix:
+            pass
+        elif fix.startswith("-") or fix.endswith("-"):
+            legit.append(fix)
+
+    file = open("database/fixes.csv", "w+", newline="")
+    file.write("fix, type\n")
+    for leg in legit:
+        if leg.startswith("-"):
+            file.write(leg + ", suffix\n")
+        elif leg.endswith("-"):
+            file.write(leg + ", prefix\n")
+    file.close()
+    
+
+scrape()
+writeCSV()
 print("\nfin")
